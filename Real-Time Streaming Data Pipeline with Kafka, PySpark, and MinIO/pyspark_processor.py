e0591598231ecc6a242533c66dd23b4f4beb3d37
Pulkit Agarwal <65941848+pulkitagar25@users.noreply.github.com>
2025-03-09 17:48:20 +0530

    Update pyspark_processor.py

--- Real-Time Streaming Data Pipeline with Kafka, PySpark, and MinIO/pyspark_processor.py
+++ Real-Time Streaming Data Pipeline with Kafka, PySpark, and MinIO/pyspark_processor.py
@@ -1,11 +1,22 @@
 from pyspark.sql import SparkSession
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# MinIO Credentials from .env
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://127.0.0.1:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+MINIO_BUCKET = os.getenv("MINIO_BUCKET_NAME", "kafka-data")
 
 # Initialize Spark Session
 spark = SparkSession.builder \
     .appName("KafkaToMinIO") \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://127.0.0.1:9000") \
-    .config("spark.hadoop.fs.s3a.access.key", Access_key) \
-    .config("spark.hadoop.fs.s3a.secret.key", Secret_key) \
+    .config("spark.hadoop.fs.s3a.endpoint", MINIO_ENDPOINT) \
+    .config("spark.hadoop.fs.s3a.access.key", MINIO_ACCESS_KEY) \
+    .config("spark.hadoop.fs.s3a.secret.key", MINIO_SECRET_KEY) \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
@@ -14,7 +25,7 @@ spark = SparkSession.builder \
 print("✅ Spark Session Initialized Successfully")
 
 # Read Data from MinIO
-df = spark.read.json("s3a://kafka-data/input/")
+df = spark.read.json(f"s3a://{MINIO_BUCKET}/input/")
 
 # Show Data
 df.show()
